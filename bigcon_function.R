6e3c88a6232bd65a5dea317835e53a1ea4b21e5f
Sohyeon Kim <ksh2904@g.skku.edu>
2019-08-06 15:52:50 +0900

    Add function

--- bigcon_function.R
+++ bigcon_function.R
@@ -13,3 +13,35 @@ Classify_Wrn_Day <- function(x) {
 Mode <- function(x) {
   table(x) %>% which.max() %>% names()
 }
+
+
+Classify_Dust_lev <- function(pm, tiny) {
+  pm <- as.numeric(pm)
+  if(tiny == 10) {
+    pm <- case_when((pm <= 30) ~ "Good", 
+                    (pm >30 & pm <=50) ~ "Moderate", 
+                    (pm >50 & pm <=75) ~ "Sens_Unhealthy", 
+                    (pm > 75 & pm <=100) ~ "Unhealthy", 
+                    (pm > 100 & pm <= 150) ~ "Very Unhealthy", 
+                    (pm > 150) ~ "Worst")
+  } else if (tiny == 25) {
+    pm <- case_when((pm <= 15) ~ "Good", 
+                    (pm>15 & pm<=25) ~ "Moderate", 
+                    (pm>25 & pm<=37) ~ "Sens_Unhealthy", 
+                    (pm>37 & pm<=50) ~ "Unhealthy", 
+                    (pm>50 & pm<=75) ~ "Very Unhealthy", 
+                    (pm>75) ~ "Worst")
+  }
+  return(pm)
+}
+
+
+
+
+
+
+
+
+
+
+
